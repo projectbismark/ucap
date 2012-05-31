8a35b8bcf45ecd4aa81e0f29ef2cd3ca31e5fb24
Michael Dandy <mdandy1988@gmail.com>
2012-05-31 08:01:45 -0400

    Add days left to the end of billing cycle

--- app/js/ui.js
+++ app/js/ui.js
@@ -335,7 +335,7 @@ function Network_networkOverview() {
 function Network_Overview_householdUsage() {
     var cap = UCapCore.household[4];
     var usage = UCapCore.household[5];
-    var template1,template2;
+    var template1,template2, template3;
     if(!isNaN(cap) && !isNaN(usage) && !(cap < 0) && !(usage < 0)){
         //No Cap
         if(cap == -1 || cap == 0){
@@ -348,15 +348,6 @@ function Network_Overview_householdUsage() {
             $('#household-usageprogress').html(template2);
             UCapViz.drawProgressBar({tar:"household-progressbar", val:0, disabled:true});
         } else {
-            //Logic to calculat days remaining goes here.
-            /*var month = new Date().getMonth();
-            var billingDate = UCapCore.household[7].split(' ').slice(0,1);
-            billingDate = billingDate[0].split('-');
-            var day = billingDate[2];
-
-            var monthLength = new Date(billingDate[0], parseInt(month), 0).getDate();
-            var daysRemaining = monthLength-day;*/
-
             var progress = Math.round((usage / cap) * 100);
             template1 = "This household currently has ";
             if(Math.round((cap - usage)/1073741824) < 1)
@@ -372,6 +363,30 @@ function Network_Overview_householdUsage() {
             $('#household-usageprogress').html(template2);
 
             UCapViz.drawProgressBar({tar:"household-progressbar", val:progress});
+			
+			//Logic to calculate days remaining goes here.
+//            var month = new Date().getMonth();
+//            var billingDate = UCapCore.household[7].split(' ').slice(0,1);
+//            billingDate = billingDate[0].split('-');
+//            var day = billingDate[2];
+//
+//            var monthLength = new Date(billingDate[0], parseInt(month), 0).getDate();
+//            var daysRemaining = monthLength-day;
+			var today = new Date();
+			var billingDay = UCapCore.household[7].split(' ');
+			billingDay = billingDay[0].split('-');
+			billingDay = new Date(billingDay[0], billingDay[1] - 1, billingDay[2]);
+			
+			var millisBetween = billingDay.getTime() - today.getTime();
+			var millisecondsPerDay = 1000 * 60 * 60 * 24;
+			var daysRemaining = Math.ceil(millisBetween / millisecondsPerDay);
+			
+			if (daysRemaining == 1)
+				template3 = daysRemaining + " day";
+			else
+				template3 = daysRemaining + " days";
+			template3 += " to end of billing cycle."
+			$('#household-billingstatus').html('<h3>'+template3+'</h3>');
         }
     }
 }
