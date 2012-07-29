4aaee3bdd7288cd5247083b2475330b217ec1354
Michael Dandy <mdandy1988@gmail.com>
2012-07-28 21:06:04 -0400

    configuration script for userpoint system

---
+++ scripts/userpoints/baseline_update.py
@@ -0,0 +1,59 @@
+import jsonrpclib
+import os
+import sys
+import time
+import datetime
+from django.conf import settings
+
+##############################################
+# This script should be run on monthly basis
+##############################################
+
+def log(message):
+	now = datetime.datetime.today()
+	sdate = now.strftime("%Y-%m-%d %H:%M:%S")
+	snow = now.strftime("%Y_%m_%d")
+	filename = 'log/baseline_update_' + snow + '.log'
+	f = open(filename, 'a')
+	f.write(sdate + '\t')
+	f.write(message)
+	f.write('\n')
+	f.close()
+
+def get_userpoint_enabled_households(client):
+	res = client.ucap.get_userpoint_enabled_households()
+	return res
+
+def get_baseline(client, house_id):
+	# Definition: Baseline is the average peak hour usage
+	# of the last month
+	res = client.ucap.get_peak_hours_usage(house_id,1)
+	peakUsage = res['total_usage']
+	numOfDays = res['num_of_days']
+	averagePeakUsage = peakUsage / numOfDays
+	return averagePeakUsage
+
+def update_baseline(client, house_id, baseline):
+	res = client.ucap.set_peak_hours_baseline(house_id, baseline)
+	return res
+
+########
+# MAIN
+########
+
+def main():
+	client = jsonrpclib.Server('https://localhost/json/json/')
+	households = get_userpoint_enabled_households(client)
+	for household in households:
+		baseline = get_baseline(client, household)
+		message = 'Baseline for ' + household + ' : ' + str(baseline)
+		print message
+		log(message)
+		
+		res = update_baseline(client, household, baseline)
+		print res
+		log (str(res[1]))
+		print '\n'
+
+if __name__ ==  '__main__':
+	main()
---
+++ scripts/userpoints/userpoint_config.py
@@ -0,0 +1,116 @@
+######################################################################
+###
+### This script enables and disables Reward system on particular
+### router
+###
+#####################################################################
+
+import jsonrpclib
+import os
+import sys
+from django.conf import settings
+
+def print_menu():
+	print 'Select a configuration (enter the number):'
+	print '\t1. Enable userpoint system'
+	print '\t2. Disable userpoint system'
+	print '\t3. Reset userpoint system (set points to zero)'
+	print '\t4. Set point per byte value'
+	print '\t0. Print configuration menu'
+	print '\t-1. Exit'
+
+def get_router_id(client, check_existance):
+	router_id = ''
+	while 1:
+		router_id = raw_input("\nEnter Router ID: ")
+		if router_id == '':
+			print 'Empty address. Input again.'
+			continue
+		
+		if check_existance:
+			result = client.ucap.check_hid_existence(router_id)
+			if result[1] == 't':
+				break
+			else:
+				print 'This Router ID does not exist. Input again.'
+				continue
+		else:
+			break
+	
+	return router_id
+	
+def enable_userpoint():
+	client = jsonrpclib.Server('https://localhost/json/json/')
+	router_id = get_router_id(client, True)
+	
+	try:
+		res = client.ucap.enable_userpoint(router_id)
+		print 'Enable userpoint...'
+		print res
+	except:
+		print 'JSONRPC Failed. Abort'
+	
+def disable_userpoint():
+	client = jsonrpclib.Server('https://localhost/json/json/')
+	router_id = get_router_id(client, True)
+	
+	try:
+		res = client.ucap.disable_userpoint(router_id)
+		print 'Disable userpoint...'
+		print res
+	except:
+		print 'JSONRPC Failed. Abort'
+	
+def reset_userpoint():
+	client = jsonrpclib.Server('https://localhost/json/json/')
+	router_id = get_router_id(client, True)
+	res = client.ucap.reset_userpoint(router_id)
+	print 'Reseting userpoint...'
+	print res
+	
+def set_pointperbyte():
+	client = jsonrpclib.Server('https://localhost/json/json/')
+	router_id = get_router_id(client, True)
+	pointperbyte = 0
+	while 1:
+		pointperbyte = raw_input("\nEnter point per byte value: ")
+		if pointperbyte == '':
+			print 'Empty value. Input again.'
+			continue
+		else:
+			break
+	
+	print 'Setting point per byte value...'
+	try:
+		res = client.ucap.set_pointperbyte(router_id, int(pointperbyte))
+	except:
+		res = 'Error: router_id: ' + router_id + ' ; point_per_byte: ' + pointperbyte
+	print res
+
+
+########
+# MAIN
+########
+def main():
+	# Config Menu
+	print_menu()
+	while 1:
+		option = raw_input("\nEnter selection (number): ")
+	
+		if option == '1':
+			enable_userpoint()
+		elif option == '2':
+			disable_userpoint()
+		elif option == '3':
+			reset_userpoint()
+		elif option == '4':
+			set_pointperbyte()
+		elif option == '0':
+			print_menu()
+		elif option == '-1':
+			break
+		else:
+			print 'Unknown configuration option'
+
+if __name__ ==  '__main__':
+	main()
